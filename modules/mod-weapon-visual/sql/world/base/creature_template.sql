83a52e14aedf77b53a33f71145f2a92f1b214c29
devovh <devovh@gmail.com>
2022-09-21 00:19:41 +0200

    * Add Weapon Visual NPC.
    * Fix SQL Gambler.

---
+++ modules/mod-weapon-visual/sql/world/base/creature_template.sql
@@ -0,0 +1,8 @@
+SET
+@Entry = 190011,
+@Name = "Eron Glowpride",
+@DisplayID = 31833;
+DELETE FROM `creature_template` WHERE `entry` = 190011;
+
+INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
+(@Entry, @DisplayID, 0, @Name, "VisualWeapon", NULL, 0, 80, 80, 2, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138936390, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 0, 'VisualWeaponNPC'); 
